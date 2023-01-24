1549b91cb8757e08b3becc93953c2a2c275b1fdf
Isabella Phung <itphung@ucsc.edu>
2023-01-24 05:13:16 +0000

    coconut tree graph detailed

--- asgn1/plot.sh
+++ asgn1/plot.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
-file_array=()
-IFS=" "
-./monte_carlo -n 10 > number_test.txt
-#read -ra file_array <<< "a b c d e f"
-#echo ${file_array[0]}
-#while read line; do echo $line; done < number_test.txt
-#while read line; do read -ra file_array <<< $line; x="${file_array[0]}"; x+=" "; x+="${file_array[1]}"; echo $x; done < number_test.txt
-while read line; do read -ra file_array <<< $line; if [[ ${file_array[4]} = 1 ]]; then line="${file_array[2]}"; line+=" "; line+="${file_array[3]}"; echo $line >> in_circle.txt; else line="${file_array[2]}"; line+=" "; line+="${file_array[3]}"; echo $line >> out_circle.txt; fi done < number_test.txt
 
+./monte_carlo -n 10000 > number_test.txt
+awk '{
+    if ($5==1) { 
+        print $3 " " $4
+    }
+}' number_test.txt > inCircle.dat
+awk '{
+    if ($5==0) {
+        print $3 " " $4
+    }
+}' number_test.txt > outCircle.dat
+awk '(NR>1){
+    diff = $2 - 3.1415926535;
+    print diff;
+}' number_test.txt > piError.dat
+
+gnuplot <<END
+    set terminal pdf size 3, 3
+    set output "circle.pdf"
+    set title "Dropped Coconuts"
+    set xlabel "x"
+    set ylabel "y"
+    set object 1 circle at graph 0,0 size graph 1 fs empty border rgb "black" arc [0:90] front
+    plot    "outCircle.dat" lt rgb "red" with dots title "", \
+            "inCircle.dat" lt rgb "blue" with dots title ""
+END
