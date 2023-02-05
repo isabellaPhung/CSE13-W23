0e94a219ead53a4a453b9b92e9f580e56e5eb328
Isabella Phung <itphung@ucsc.edu>
2023-02-05 18:22:04 +0000

    debbuged sorting algorithms

--- asgn3/batcher.c
+++ asgn3/batcher.c
@@ -6,27 +6,30 @@ void batcher_sort(Stats *stats, uint32_t *A, uint32_t n){
     if(n == 0){
         return;
     }
-
-    uint32_t t = 32;
-
+    uint32_t t = 4;
     uint32_t p = 1 << (t-1);
     uint32_t q;
     uint32_t r = 0;
     uint32_t d; 
     while(p > 0){
         q = 1 << (t - 1);
+
         r = 0;
         d = p;
-        for(uint32_t i = 0; i < n-d; i++){
-            if ((i & p) == r){
-                if(cmp(stats, A[i], A[i+d])){
-                    swap(stats, &A[i], &A[i+d]);
+        
+        while(d > 0){
+            uint32_t temp = n-d;
+            for(uint32_t i = 0; i < temp; i++){
+                if ((i & p) == r){
+                    if(cmp(stats, A[i], A[i+d]) == 1){
+                        swap(stats, &A[i], &A[i+d]);
+                    }
                 }
             }
-       }
-       d = (q - p);
-       q >>= 1;
-       r=p;
+            d = (q - p);
+            q >>= 1;
+            r=p; 
+            }     
+        p >>= 1;
     }
-    p >>= 1;
 }
--- asgn3/set.c
+++ asgn3/set.c
@@ -2,42 +2,58 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-typedef uint32_t Set{
-
-}
+typedef uint32_t Set;
 
 Set set_empty(void){
-
+    Set s = 0;
+    return s;
 }
 
 Set set_universal(void){
-
+    Set s = 4294967295;
+    return s;
 }
 
 Set set_insert(Set s, uint8_t x){
-
+    int one = 1;
+    int shift = one << x;
+    s = s | shift;
+    return s;
 }
 
 Set set_remove(Set s, uint8_t x){
-
+    int zero = 0;
+    int shift = zero << x;
+    s = s | shift;
+    return s;
 }
 
 bool set_member(Set s, uint8_t x){
-
+    int one = 1;
+    int shift = one << x;
+    int checkVal = 0;
+    checkVal = s & shift;
+    if(checkVal != 0){
+        return true;
+    }
+    return false;
 }
 
 Set set_union(Set s, Set t){
-
+    Set newSet = s | t;
+    return newSet;
 }
 
 Set set_intersect(Set s, Set t){
+    Set newSet = s & t;
+    return newSet;
 
 }
 
 Set set_difference(Set s, Set t){
-
+    return s & ~t;
 }
 
 Set set_complement(Set s){
-
+    return ~s;
 }
